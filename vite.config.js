3b4b03f12cb9f56883e41c9872fe49df58ac7a22
Mit Gohel <166828574+MitGohel@users.noreply.github.com>
2025-09-15 21:27:04 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/",   // 👈 Add this line (repo name)
   plugins: [react()],
 })
